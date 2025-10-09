eee73cbee14d9f2aaaff771955c7650da307f236
sheikhlimon <sheikhlimon404@gmail.com>
2025-10-09 23:43:23 +0600

    Display OpsiMate version in sidebar footer

--- apps/client/src/hooks/queries/index.ts
+++ apps/client/src/hooks/queries/index.ts
@@ -1,13 +1,15 @@
 // Export all query hooks
-export * from './services';
-export * from './alerts';
-export * from './providers';
-export * from './tags';
-export * from './integrations';
-export * from './views';
-export * from './users';
-export * from './audit';
-export * from './custom-fields';
+export * from "./services";
+export * from "./alerts";
+export * from "./providers";
+export * from "./tags";
+export * from "./integrations";
+export * from "./views";
+export * from "./users";
+export * from "./audit";
+export * from "./custom-fields";
+export * from "./version";
 
 // Export query keys
-export { queryKeys } from './queryKeys'; 
+export { queryKeys } from "./queryKeys";
+
