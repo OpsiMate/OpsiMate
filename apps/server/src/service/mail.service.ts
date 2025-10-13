76883e200b2d4d8c87bee421cb9e0a86458d7460
raphaeltx <teixeira.e.a.raphael@gmail.com>
2025-10-13 00:36:49 -0300

    Add mail service

---
+++ apps/server/src/service/mail.service.ts
@@ -0,0 +1,64 @@
+import { Logger } from "@OpsiMate/shared";
+import nodemailer from "nodemailer";
+
+interface SendMailOptions {
+  to: string;
+  subject: string;
+  html?: string;
+  text?: string;
+}
+
+const logger = new Logger("service/mail.service");
+
+/**
+ * Service for sending emails using nodemailer
+ */
+export class MailService {
+  private transporter: nodemailer.Transporter = null as any;
+
+  constructor() {
+    this.initTransporter();
+  }
+
+  /**
+   * Initialize the nodemailer transporter with SMTP settings
+   */
+  initTransporter() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT),
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    this.transporter
+      .verify()
+      .then(() => {
+        logger.info("MailService: SMTP transporter is ready to send emails");
+      })
+      .catch((error) => {
+        logger.error("MailService: Error verifying SMTP transporter", error);
+      });
+  }
+
+  /**
+   * Send an email
+   * @param options
+   */
+  async sendMail(options: SendMailOptions): Promise<void> {
+    try {
+      await this.transporter.sendMail({
+        from: '"OpsiMate" <no-reply@opsimate.com>',
+        to: options.to,
+        subject: options.subject,
+        html: options.html,
+      });
+    } catch (error) {
+      logger.error("MailService: Error sending email", error);
+      throw new Error("Failed to send email");
+    }
+  }
+}
