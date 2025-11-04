7242b8d0844a3c20d0a0b83551a45e3ae0f5e410
saharlevy <le.sahar@gmail.com>
2025-11-04 22:24:32 +0200

    fix format issues

--- packages/custom-actions/eslint.config.mjs
+++ packages/custom-actions/eslint.config.mjs
@@ -15,7 +15,10 @@ export default [
     },
     rules: {
       "@typescript-eslint/no-floating-promises": "error",
-      "@typescript-eslint/no-unused-vars": ['warn', { argsIgnorePattern: '^_' }],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
     },
   },
 ];
