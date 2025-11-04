418490e79d18448ec45117360e309a4f1ee36c14
saharlevy <le.sahar@gmail.com>
2025-11-04 22:31:07 +0200

    change eslint

--- packages/custom-actions/eslint.config.mjs
+++ packages/custom-actions/eslint.config.mjs
@@ -3,6 +3,9 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
+  {
+    ignores: ["dist"],
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
